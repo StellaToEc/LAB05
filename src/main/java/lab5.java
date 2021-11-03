dce48d7a2c5d98400596fbb32e6b16bd0bef4860
stell <cristinatoes@gmail.com>
2021-11-03 09:49:17 -0600

    finalwhitout test vectors

--- src/main/java/lab5.java
+++ src/main/java/lab5.java
@@ -1,17 +1,17 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.Base64;
 import java.util.Scanner;
-import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
-import javax.crypto.NoSuchPaddingException;
-import javax.crypto.SecretKey;
+import javax.crypto.*;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
@@ -73,17 +73,32 @@ public class lab5 {
         }
     }
 
-    public IvParameterSpec createIv() {
+    static IvParameterSpec createIv() {
         byte[] iv = new byte[16];
         new SecureRandom().nextBytes(iv);
         return new IvParameterSpec(iv);
     }
 
-    static void encrypt(String keyfile, String filename, int mode, int elec) throws NoSuchPaddingException, NoSuchAlgorithmException, IOException {
+    static void encrypt(String keyfile, String filename, int mode, int elec) throws NoSuchPaddingException, NoSuchAlgorithmException, IOException, BadPaddingException, IllegalBlockSizeException, InvalidAlgorithmParameterException, InvalidKeyException {
         String instance = "AES/CBC/PKCS5Padding";
-
+        String plainText;
+        String cypherText;
+        byte[] ct;
         SecretKey key= new SecretKeySpec(Base64.getDecoder().decode(readText(keyfile, 1)), "AES" );
+        IvParameterSpec iv;
 
+        if(elec==1){
+            plainText = readText(filename,1);
+            iv=createIv();
+        }else{
+            plainText = readText(filename,2);
+            iv = new IvParameterSpec(plainText.substring(0,15).getBytes(StandardCharsets.UTF_8)); //default
+            int keysize = key.toString().length();
+            if(keysize==23)
+                iv = new IvParameterSpec(plainText.substring(0,23).getBytes(StandardCharsets.UTF_8));
+            if(keysize==31)
+                iv = new IvParameterSpec(plainText.substring(0,31).getBytes(StandardCharsets.UTF_8));
+        }
         switch(mode){
             case 1 -> instance="AES/CBC/PKCS5Padding";
             case 2 -> instance="AES/CTR/PKCS5Padding";
@@ -91,12 +106,24 @@ public class lab5 {
         }
 
         Cipher cipher= Cipher.getInstance(instance);
+        cipher.init(elec,key,iv);
+        ct= cipher.doFinal(plainText.getBytes(StandardCharsets.UTF_8));
 
+        if(elec==1){
+            cypherText = new String(iv.toString())+ new String(ct);
+            saveText(cypherText,filename,1);
+        }else {
+            cypherText = new String(ct);
+            saveText(cypherText,filename,2);
+        }
     }
 
 
-    public static void main (String[]args) throws IOException{
+    public static void main (String[]args) throws IOException, NoSuchPaddingException,NoSuchAlgorithmException, BadPaddingException, IllegalBlockSizeException, InvalidKeyException, InvalidAlgorithmParameterException{
         Scanner reader = new Scanner(System.in);
+        String keyfile;
+        String namefile;
+        int mode;
 
         System.out.println("to encrypt press 1 to decrypt 2");
         int elec = reader.nextInt();
@@ -104,10 +131,26 @@ public class lab5 {
 
         if(elec==1){
             System.out.println("Write the name file for save the key:");
-            String namefile= reader.nextLine();
+            keyfile= reader.nextLine();
             System.out.println("Choose key size(128, 192 or 256):");
             int size = reader.nextInt();
-            createKey(size,namefile);
+
+            createKey(size,keyfile);
+
+            System.out.println("Write the name file to encrypt:");
+            namefile= reader.nextLine();
+            System.out.println("Choose a mode 1-CBC 2-CTR 3-CFB:");
+            mode = reader.nextInt();
+
+            encrypt(keyfile,namefile,mode,elec);
+        }else{
+            System.out.println("Write the name file for read the key:");
+            keyfile= reader.nextLine();
+            System.out.println("Write the name file to decrypt:");
+            namefile= reader.nextLine();
+            System.out.println("Choose a mode 1-CBC 2-CTR 3-CFB:");
+            mode = reader.nextInt();
+            encrypt(keyfile,namefile,mode,elec);
         }
     }
 }
